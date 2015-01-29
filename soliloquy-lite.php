15de24d3d983d29bcba2b44d8e5ef0d0058c6979
n7studios <n7studios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 16:24:25 +0000

    Tagging version 2.1.9
    git-svn-id: https://plugins.svn.wordpress.org/soliloquy-lite/tags/2.1.9@1078342 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- soliloquy-lite.php
+++ soliloquy-lite.php
@@ -5,7 +5,7 @@
  * Description: Soliloquy is best responsive WordPress slider plugin. This is the lite version.
  * Author:      Thomas Griffin
  * Author URI:  http://thomasgriffinmedia.com
- * Version:     2.1.7
+ * Version:     2.1.9
  * Text Domain: soliloquy
  * Domain Path: languages
  *
@@ -54,7 +54,7 @@ class Soliloquy_Lite {
      *
      * @var string
      */
-    public $version = '2.1.7';
+    public $version = '2.1.9';
 
     /**
      * The name of the plugin.
